129c543aacdd25d527a8bbe01356a3f0b64ed1cb
Marcus Lacerda <marcus.lacerda@gmail.com>
2016-11-26 14:33:05 -0200

    change log level to INFO

--- jobs/script_stack.py
+++ jobs/script_stack.py
@@ -8,7 +8,7 @@ FORMAT = '%(name)s %(levelname)-5s %(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('stack')
 logger.addHandler(logging.NullHandler())
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 logging.getLogger('elasticsearch').setLevel(logging.ERROR)
 
 stack = Stack()
